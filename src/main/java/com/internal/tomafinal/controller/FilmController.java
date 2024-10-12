ff22d10d6f80b528b78ee0b296a3ab352821083c
eva <evahuertacarb@gmail.com>
2024-10-12 20:17:15 +0200

    CRUD controller para películas

--- src/main/java/com/internal/tomafinal/controller/FilmController.java
+++ src/main/java/com/internal/tomafinal/controller/FilmController.java
@@ -1,4 +1,38 @@
 package com.internal.tomafinal.controller;
 
+import com.internal.tomafinal.controller.model.FilmDTO;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/films")
 public class FilmController {
+
+    @GetMapping
+    public List<FilmDTO> getFilms(){
+        return List.of();
+    }
+
+    @GetMapping("/{name}")
+    public FilmDTO getFilm(@PathVariable String name){
+        return null;
+    }
+
+    @PostMapping
+    public FilmDTO postFilm(@RequestBody FilmDTO filmDTO){
+        return null;
+    }
+
+    @PutMapping ("/{name}")
+    public FilmDTO putFilm(@RequestBody FilmDTO filmDTO){
+        return null;
+    }
+
+    @DeleteMapping("/{name}")
+    public String deleteFilm(@PathVariable String name){
+        return "";
+    }
+
+
 }
